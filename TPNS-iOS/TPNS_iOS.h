bbac43e9748acb106bb1ce56389cb060621817af
zoeyliang@tencent.com <zoeyliang@tencent.com>
2019-10-29 14:32:24 +0800

    TPNS-iOS 1.2.4.0

---
+++ TPNS-iOS/TPNS_iOS.h
@@ -0,0 +1,19 @@
+//
+//  TPNS_iOS.h
+//  TPNS-iOS
+//
+//  Created by admin on 2019/10/29.
+//  Copyright © 2019年 XG of Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPNS_iOS.
+FOUNDATION_EXPORT double TPNS_iOSVersionNumber;
+
+//! Project version string for TPNS_iOS.
+FOUNDATION_EXPORT const unsigned char TPNS_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPNS_iOS/PublicHeader.h>
+
+
